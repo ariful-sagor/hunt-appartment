95bd2132139799e4e979c9c5bc0a0356cbc3d02f
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-11-15 20:58:05 +0600

    add backend

--- src/Components/Bookings/AddRent/AddRent.js
+++ src/Components/Bookings/AddRent/AddRent.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddRent = () => {
-    const handleBlur= e =>{
-        console.log(e)
+    const [info, setInfo] = useState({});
+    const [file, setFile] = useState(null);
+    const handleBlur = e => {
+        const newInfo = { ...info };
+        newInfo[e.target.name] = e.target.value;
+        setInfo(newInfo);
     }
 
     const handleFileChange = (e) => {
-        console.log(e)
-
+        const newFile = e.target.files[0];
+        setFile(newFile);
     }
 
     const handleSubmit = (e) => {
-        console.log(e)
-    }
+        const formData = new FormData()
+        console.log(info);
+        formData.append('file', file);
+        formData.append('title', info.title);
+        formData.append('location', info.location);
+        formData.append('bathroom', info.bathroom);
+        formData.append('price', info.price);
+        formData.append('bedroom', info.bedroom);
 
+        fetch('https://localhost:5000/addRentHouse', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert('Service Added successfully!');
+                console.log(data);
+                e.target.reset();
+            })
+            .catch(error => {
+                console.error(error);
+                alert('Fill all fields properly.');
+            })
+        e.preventDefault();
+        
+        }
 
     return (
         <section id="order" className="p-4">
@@ -23,18 +50,18 @@ const AddRent = () => {
                     <div className="row">
                         <div className="col-md-8 col-lg-6">   
                             <label ><b>Service Title</b></label>               
-                            <input  type="text" onBlur={handleBlur} name="service" placeholder="Enter title" className="form-control " required />  <br />
+                            <input  type="text" onBlur={handleBlur} name="title" placeholder="Enter title" className="form-control " required />  <br />
                             <label ><b>Location</b></label>                 
-                            <input type="text" onBlur={handleBlur} name="service" placeholder="Enter Location" className="form-control" required /> <br />
+                            <input type="text" onBlur={handleBlur} name="location" placeholder="Enter Location" className="form-control" required /> <br />
                             <label ><b>No. of Bedrooms</b></label>                 
-                            <input type="text" onBlur={handleBlur} name="service" placeholder="Enter Bedrooms" className="form-control" required />                          
+                            <input type="text" onBlur={handleBlur} name="bedroom" placeholder="Enter Bedrooms" className="form-control" required />                          
                         </div>
                         <div className="col-md-4 col-lg-6">         
 
                             <label ><b>Price</b></label>               
-                            <input  type="text" onBlur={handleBlur} name="service" placeholder="Enter Price" className="form-control" required />  <br />
+                            <input  type="text" onBlur={handleBlur} name="price" placeholder="Enter Price" className="form-control" required />  <br />
                             <label ><b>No. of Bathrooms</b></label>               
-                            <input  type="text" onBlur={handleBlur} name="service" placeholder="Enter Bathrooms" className="form-control" required /> <br />                                                      
+                            <input  type="text" onBlur={handleBlur} name="bathroom" placeholder="Enter Bathrooms" className="form-control" required /> <br />                                                      
                             <div className="col-6 my-1">
                                 <label><b>Thumbnail</b></label>
                                 <input type="file" onChange={handleFileChange} name="photo" />
@@ -47,7 +74,7 @@ const AddRent = () => {
             <div className="row">
                     <div className="col-md-10"></div>
                     <div className="col-md-2">
-                        <button  className="btn btn-success ml-3 px-4">Submit</button> 
+                        <input type="submit" className="btn btn-success ml-3 px-4" value="Submit"/>
                     </div>    
                 </div>  
         </section>
