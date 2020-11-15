c9ee095b37ba9c92e9c77e26c4d48fa09f9e870a
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-11-16 00:16:52 +0600

    home details page add

---
+++ src/Components/Home/HomeDetails/HomeDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+
+const HomeDetails = () => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    );
+};
+
+export default HomeDetails;
