b6e327de86a33fb367204e77c41224662590d562
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-11-16 22:45:57 +0600

    dashboard part complete

--- src/Components/Bookings/List/ListDetails.js
+++ src/Components/Bookings/List/ListDetails.js
@@ -5,10 +5,10 @@ const ListDetails = ({ data }) => {
     return (
         <tbody>
             <tr>
-                <td>{data.name}</td>
-                <td>{data.email}</td>
-                <td>{data.phone}</td>
-                <td>{data.message}</td>
+                <td><b>{data.name}</b></td>
+                <td><b>{data.email}</b></td>
+                <td><b>{data.phone}</b></td>
+                <td><b>{data.message}</b></td>
                 <td className="text-success">Done</td>
             </tr>
         </tbody>
